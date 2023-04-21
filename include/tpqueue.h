d6a9cb5b02d9652ba085905b42841c26a661a1d4
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-04-21 12:27:55 +0300

    b53

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,69 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct Item {
+        SYM data;
+        Item* next;
+    };
+    Item* first;
+
+ public:
+    TPQueue() : first(nullptr) {}
+    ~TPQueue() {
+        while (!isEmpty()) {
+            Item* temp = first;
+            first = first->next;
+            delete temp;
+        }
+    }
+    bool isEmpty() {
+        return first == nullptr;
+    }
+    void push(const SYM& data) {
+        Item* newItem = new Item;
+        newItem->data = data;
+        newItem->next = nullptr;
+
+        if (isEmpty()) {
+            first = newItem;
+            return;
+        }
+
+        if (data.prior > first->data.prior) {
+            newItem->next = first;
+            first = newItem;
+            return;
+        }
+
+        Item* current = first;
+        while (current->next != nullptr && current->next->data.prior >= data.prior) {
+            current = current->next;
+        }
+
+        newItem->next = current->next;
+        current->next = newItem;
+    }
+
+    SYM pop() {
+        if (isEmpty())
+            throw std::string("Empty");
+        SYM result = first->data;
+        Item* temp = first;
+        first = first->next;
+        delete temp;
+        return result;
+    }
+    void print() {
+        if (isEmpty())
+            std::cout << "Empty";
+        Item* temp = first;
+        while (temp) {
+            std::cout << temp->data.ch << " (pr: " << temp->data.prior << ")" << std::endl;
+            temp = temp->next;
+
+        }
+    }
 };
 
 struct SYM {

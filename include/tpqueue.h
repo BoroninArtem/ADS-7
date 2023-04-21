93069a1a1541693e2246587f4ce3b49cde86e1d5
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-04-21 12:58:01 +0300

    b53

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,9 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
+#include <string>
 template<typename T>
 class TPQueue {
-
  private:
     struct Item {
         T data;

9173a05bdb311a21ce6a0b560f8095c06155fe21
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-04-21 12:55:44 +0300

    b53

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,61 +1,48 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+
 template<typename T>
 class TPQueue {
- 
+
  private:
     struct Item {
         T data;
         Item* next;
     };
-    Item* first, last;
+    Item* first;
+    Item* last;
     Item* create(T data) {
         Item* item = new Item;
         item->data = data;
         item->next = nullptr;
         return item;
     }
- 
+
  public:
-    TPQueue() :first(nullptr), tail(nullptr) {}
+    TPQueue() :first(nullptr), last(nullptr) {}
     ~TPQueue() {
         while (!isEmpty())
             pop();
     }
     void push(const T& data) {
-        if (!isEmpty()) {
+        if (isEmpty()) {
+            first = create(data);
+            last = first;
+        } else {
             Item* begin = first;
             Item* newItem = create(data);
-            if (first == tail) {
-                if (data.prior > first->data.prior) {
-                    newItem->next = first;
-                    first = newItem;
-                } else {
-                    first->next = newItem;
-                    tail = newItem;
-                }
+            if (data.prior > first->data.prior) {
+                newItem->next = first;
+                first = newItem;
             } else {
-                Item* prev = nullptr;
-                while (begin != tail && data.prior <= begin->data.prior) {
-                    prev = begin;
+                while (begin->next != nullptr && data.prior <= begin->next->data.prior)
                     begin = begin->next;
-                }
-                if (data.prior > begin->data.prior) {
-                    newItem->next = begin;
-                    if (prev) {
-                        prev->next = newItem;
-                    } else {
-                        first = newItem;
-                    }
-                } else {
-                    tail->next = newItem;
-                    tail = newItem;
-                }
+                newItem->next = begin->next;
+                begin->next = newItem;
+                if (begin == last)
+                    last = newItem;
             }
-        } else {
-            first = create(data);
-            tail = first;
         }
     }
     T pop() {
@@ -76,4 +63,5 @@ struct SYM {
     char ch;
     int prior;
 };
+
 #endif  // INCLUDE_TPQUEUE_H_
